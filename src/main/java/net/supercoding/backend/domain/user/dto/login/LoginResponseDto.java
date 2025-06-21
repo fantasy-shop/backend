9616fc8e0b0bb111a07c4a69c1a725f08a04ed55
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-06-21 22:00:42 +0900

    modify : - login 토큰만 보내기

--- src/main/java/net/supercoding/backend/domain/user/dto/login/LoginResponseDto.java
+++ src/main/java/net/supercoding/backend/domain/user/dto/login/LoginResponseDto.java
@@ -1,41 +1,38 @@
 package net.supercoding.backend.domain.user.dto.login;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
 import net.supercoding.backend.domain.user.entity.User;
-
-import java.time.LocalDateTime;
 
 @Getter
 @Builder
 public class LoginResponseDto {
     private String token;
-
-    private Long userPk;
-    private String email;
-    private String userName;
-    private String phoneNumber;
-    private String address;
-    private String profileImageUrl;
-    private LocalDateTime createAt;
-    private String userStatus;
-    @JsonProperty("isAdmin")
-    private boolean isAdmin;
+//
+//    private Long userPk;
+//    private String email;
+//    private String userName;
+//    private String phoneNumber;
+//    private String address;
+//    private String profileImageUrl;
+//    private LocalDateTime createAt;
+//    private String userStatus;
+//    @JsonProperty("isAdmin")
+//    private boolean isAdmin;
 
 
     public static LoginResponseDto of(User user, String token) {
         return LoginResponseDto.builder()
                 .token(token)
-                .userPk(user.getUserPk())
-                .email(user.getEmail())
-                .userName(user.getUserName())
-                .address(user.getAddress())
-                .phoneNumber(user.getPhoneNumber())
-                .createAt(LocalDateTime.now())
-                .userStatus(user.getUserStatus().name())
-                .isAdmin(user.getIsAdmin())
-                .profileImageUrl(user.getProfileImageUrl())
+//                .userPk(user.getUserPk())
+//                .email(user.getEmail())
+//                .userName(user.getUserName())
+//                .address(user.getAddress())
+//                .phoneNumber(user.getPhoneNumber())
+//                .createAt(LocalDateTime.now())
+//                .userStatus(user.getUserStatus().name())
+//                .isAdmin(user.getIsAdmin())
+//                .profileImageUrl(user.getProfileImageUrl())
                 .build();
     }
 }
