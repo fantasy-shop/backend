929dd50a662a437262b7210f640c3d1b1a16bb7c
hello22433 <104301546+hello22433@users.noreply.github.com>
2025-06-23 08:45:39 +0900

    modify : 테스트서버로 파일 저장경로 변경

--- src/main/java/net/supercoding/backend/domain/user/service/UserService.java
+++ src/main/java/net/supercoding/backend/domain/user/service/UserService.java
@@ -87,8 +87,8 @@ public class UserService {
                 // String projectRoot = System.getProperty("user.dir");
                 // String existingImagePath = projectRoot + "/src/main/resources/static" + existingImageUrl;
                 // 변경: EC2 등 실제 저장 경로로 지정
-//                String existingImagePath = "/home/ec2-user/images" + existingImageUrl;
-                String existingImagePath = "/home/ubuntu/www/fantasyshop/assets/images" + existingImageUrl;
+                String existingImagePath = "/home/ec2-user/images" + existingImageUrl;
+//                String existingImagePath = "/home/ubuntu/www/fantasyshop/assets/images" + existingImageUrl;
 
                 File existingFile = new File(existingImagePath);
                 if (existingFile.exists()) existingFile.delete();
@@ -98,8 +98,8 @@ public class UserService {
             // String projectRoot = System.getProperty("user.dir");
             // String uploadDirPath = projectRoot + "/src/main/resources/static/images/" + today;
             // 변경: EC2 등 실제 저장 경로로 지정
-//            String uploadDirPath = "/home/ec2-user/images/" + today; // 변경된 부분
-            String uploadDirPath = "/home/ubuntu/www/fantasyshop/assets/images/" + today; // 변경된 부분
+            String uploadDirPath = "/home/ec2-user/images/" + today; // 변경된 부분
+//            String uploadDirPath = "/home/ubuntu/www/fantasyshop/assets/images/" + today; // 변경된 부분
 
             File uploadDir = new File(uploadDirPath);
             if (!uploadDir.exists()) uploadDir.mkdirs();
@@ -115,7 +115,8 @@ public class UserService {
                     .toFile(savedFile);
 
 
-            String imageUrl = today + "/" + savedFileName;  // 상대경로
+            String imageUrl = "/images" + today + "/" + savedFileName;  // 상대경로
+//            String imageUrl = today + "/" + savedFileName;  // 상대경로
             user.setProfileImageUrl(imageUrl);
         }
 
